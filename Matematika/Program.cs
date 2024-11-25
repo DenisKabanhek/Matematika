1863defb7aabaa480f4006d0565fc06d2d6c7074
Denis <sadovnikden14@gmail.com>
2024-11-25 18:34:43 +0400

    Initial commit

---
+++ Matematika/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//Круг
+double a = 314.0;
+double b = 3.14;
+double c = a / b; //
+Console.WriteLine(c);
+//Треугольник 
+int x = 6;
+int y = 9;
+int z = x * y; //
+Console.WriteLine(z);
+//Прямоугольник 
+int e = 11;
+int r = 13;
+int d = e * r; //
+Console.WriteLine(d);
+
+
+   
+                       
+
+
+
+
+
